0e8a30c2001494d8a3ba576a3feead6ee7dc2c3b
vasus2 <vasus2@illinois.edu>
2023-12-21 11:09:58 -0800

    calculations done. need to add value return then program is done!

---
+++ api_arb_calculator.py
@@ -0,0 +1,121 @@
+import requests
+
+
+def arb_calc(home_odds, away_odds, total_wager):
+    home_wager = total_wager * (home_odds/(away_odds + home_odds))
+    away_wager = total_wager * (away_odds/(away_odds + home_odds))
+    return home_wager, away_wager
+
+def find_arbitrage_opportunities(game_data):
+    bookmaker_home_price = {}
+    bookmaker_away_price = {}
+
+
+    for bookmaker in game_data['bookmakers']:
+        #print(bookmaker)
+        for market in bookmaker['markets']:
+            outcomes = market['outcomes']
+            bookmaker_home_price[bookmaker['key']] = int(outcomes[0]['price'])
+            bookmaker_away_price[bookmaker['key']] = int(outcomes[1]['price'])
+
+    
+
+    max_away_bookie = max(bookmaker_away_price, key = lambda key: bookmaker_away_price[key])
+    max_home_bookie = max(bookmaker_home_price, key = lambda key: bookmaker_home_price[key])
+    max_home = int(bookmaker_home_price[max_home_bookie])
+    max_away = int(bookmaker_away_price[max_away_bookie])
+    print(max_home_bookie, max_home)
+    print(max_away_bookie, max_away)
+
+    home_odds = 0
+    away_odds = 0
+    if max_home < 0:
+        home_odds = (abs(max_home)/(abs(max_home) + 100)) * 100
+    else: home_odds = (100/(abs(max_home) + 100)) * 100
+
+    if max_away < 0:
+        away_odds = (abs(max_away)/(abs(max_away) + 100)) * 100
+    else: away_odds = (100/(abs(max_away) + 100)) * 100
+
+    if home_odds + away_odds < 100:
+        home_wager, away_wager = arb_calc(home_odds, away_odds, 100)
+        print(home_wager, away_wager)
+
+
+
+# An api key is emailed to you when you sign up to a plan
+# Get a free API key at https://api.the-odds-api.com/
+API_KEY = '962ccd9bb90fbd5aacf0dfb232570710'
+
+SPORT = 'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
+
+REGIONS = 'us' # uk | us | eu | au. Multiple can be specified if comma delimited
+
+MARKETS = 'h2h' # h2h | spreads | totals. Multiple can be specified if comma delimited
+
+ODDS_FORMAT = 'american' # decimal | american
+
+DATE_FORMAT = 'iso' # iso | unix
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# First get a list of in-season sports
+#   The sport 'key' from the response can be used to get odds in the next request
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+'''
+sports_response = requests.get(
+    'https://api.the-odds-api.com/v4/sports', 
+    params={
+        'api_key': API_KEY
+    }
+)
+
+
+if sports_response.status_code != 200:
+    print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
+
+else:
+    print('List of in season sports:', sports_response.json())
+
+'''
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
+# This will deduct from the usage quota
+# The usage quota cost = [number of markets specified] x [number of regions specified]
+# For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+odds_json = ""
+
+odds_response = requests.get(
+    f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+    params={
+        'api_key': API_KEY,
+        'regions': REGIONS,
+        'markets': MARKETS,
+        'oddsFormat': ODDS_FORMAT,
+        'dateFormat': DATE_FORMAT,
+    }
+)
+if odds_response.status_code != 200:
+    print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
+else:
+    odds_json = odds_response.json()
+    print('Number of events:', len(odds_json))
+    #print(odds_json)
+    for game in odds_json:
+        find_arbitrage_opportunities(game)
+
+    
+
+    # Check the usage quota
+    print('Remaining requests', odds_response.headers['x-requests-remaining'])
+    print('Used requests', odds_response.headers['x-requests-used'])
+
+
+
--- web_scraping.py
+++ web_scraping.py
@@ -10,108 +10,63 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
-driver.get("https://www.oddschecker.com/us/football/college-football")
+def get_odds(link):
+
+    driver.get(link)
 
 
-driver.maximize_window()
+    driver.maximize_window()
 
 
-data = driver.find_element("xpath", 
-                            "/html/body/div[5]/div[3]/div[1]/div[1]")
+    data = driver.find_element("xpath", 
+                                "/html/body/div[5]/div[3]/div[1]/div[1]")
 
-text_data = data.text
+    text_data = data.text
 
-# Split the text into lines
-lines = text_data.strip().split('\n')
+    # Split the text into lines
+    lines = text_data.strip().split('\n')
 
-# Remove empty lines
-lines = [line for line in lines if line.strip()]
+    # Remove empty lines
+    lines = [line for line in lines if line.strip()]
 
-# Create an empty DataFrame with the desired column names
-#df = pd.DataFrame(columns=["Date", "Time", "Game", "Moneyline", "Spread", "Total"])
-df = pd.DataFrame(columns=["Date", "Time", "Game", "Moneyline"])
+    # Create an empty DataFrame with the desired column names
+    df = pd.DataFrame(columns=["Date", "Time", "Game", "Moneyline", "Spread", "Total"])
+    #df = pd.DataFrame(columns=["Date", "Time", "Game", "Moneyline", "Arb Opp"])
+    arb_opp_df =  pd.DataFrame(columns=["Date", "Time", "Game", "Moneyline", "Arb Opp"])
 
-# Create a list to store the new rows
-new_rows = []
+    # Create a list to store the new rows
+    new_rows = []
+    arb_rows = []
 
-# Iterate over the data list, adding each item to the DataFrame
-for i in range(len(lines)):
-  if lines[i] == 'MONEYLINE':
-    #print(lines[i+1], lines[i+2], lines[i+3], lines[i+4])
-    date = lines[i-1]
-    game = lines[i+4] + " " + lines[i+5]
-    moneyline = lines[i+6] + " : " + lines[i+7]
-    #spread = lines[i+8:i+12]
-    #total = lines[i+12:i+16]
-    time = lines[i+3]
-    #new_rows.append({"Date": date, "Time": time, "Game": game, "Moneyline": moneyline, "Spread": spread, "Total": total})
-    new_rows.append({"Date": date, "Time": time, "Game": game, "Moneyline": moneyline})
-      
-
-
-df = pd.concat([df, pd.DataFrame(new_rows)], ignore_index=True)
-# Print the DataFrame
-print(df)
-
-
-# Extract relevant information
-
-'''data = []
-current_match = None
-
-for line in lines:
-    if line in ["MONEYLINE", "SPREAD", "TOTAL"]:
-        continue  # Skip section headers
-
-    if line.endswith("EST"):
-        match_date = lines[lines.index(line) - 1]
-        match_time = line
-        current_match = {"Date": match_date, "Time": match_time}
-    elif current_match:
-        parts = line.split()
-        if len(parts) == 2:
-            current_match[parts[0]] = parts[1]
-        elif len(parts) == 3:
-            current_match[parts[0] + "_Odds"] = parts[1]
-            current_match[parts[0]] = parts[2]
-
-        if line.startswith("U "):
-            data.append(current_match)
-            current_match = None
-
-# Create a DataFrame
-df = pd.DataFrame(data)
-
-# Display the DataFrame
-print(df)
-'''
+    # Iterate over the data list, adding each item to the DataFrame
+    for i in range(len(lines)):
+        if lines[i] == 'MONEYLINE':
+            date = lines[i-1]
+            game = lines[i+4] + " " + lines[i+5]
+            moneyline = lines[i+6] + " : " + lines[i+7]
+            spread = lines[i+8:i+12]
+            total = lines[i+12:i+16]
+            time = lines[i+3]
+            if int(lines[i+6]) + int(lines[i+7]) > 0:
+                arb_opp = "yes"
+                arb_rows.append({"Date": date, "Time": time, "Game": game, "Moneyline": moneyline, "Arb Opp": arb_opp})
+            else: 
+                arb_opp = "no"
+                new_rows.append({"Date": date, "Time": time, "Game": game, "Moneyline": moneyline, "Spread": spread, "Total": total})
 
 
-'''
-links = driver.find_elements("xpath", "//a[@href]")
-
-for link in links:
-    if "Books" in link.get_attribute("innerHTML"):
-        link.click()
-        break
-
-book_links = driver.find_elements("xpath", 
-                                  "//div[contains(@class, 'elementor-column-wrap')][.//h2[text()[contains(.,'7 IN 1')]]][count(.//a)=2]//a")
-
-book_links[0].click()
-
-driver.switch_to.window(driver.window_handles[1])
-
-time.sleep(3)
-
-buttons = driver.find_elements("xpath", "//a[.//span[text()[contains(., 'Paperback')]]]//span[text()[contains(., '$')]]")
-
-for button in buttons:
-    print(button.get_attribute("innerHTML"))
+    df = pd.concat([df, pd.DataFrame(new_rows)], ignore_index=True)
+    arb_opp_df = pd.concat([arb_opp_df, pd.DataFrame(arb_rows)], ignore_index=True)
+    # Print the DataFrame
+    print(df)
+    print(arb_opp_df)
+    driver.quit()
 
 
-driver.quit()
+get_odds("https://www.oddschecker.com/us/football/college-football")
 
-'''
+
+
+
 
 
